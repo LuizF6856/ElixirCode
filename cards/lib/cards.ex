5d9770ab75809ea44daac29cf6c4724347926049
LuizF6856 <luizfellipe6843@gmail.com>
2022-07-05 15:06:18 -0300

    26. Writing and generating documentation

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,4 +1,8 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and hadling a deck of cards
+  """
+
     def create_deck do
       values = ["Ace", "Two", "Three", "Four", "Five"]
       suits = ["Tree", "Spades", "Clubs", "Hearts", "Diamonds"]
@@ -11,4 +15,34 @@ defmodule Cards do
     def contains?(deck, cards), do: Enum.member?(deck, cards)
 
     def deal(deck, hand_size), do: Enum.split(deck, hand_size)
+
+    def save(deck, filename) do
+      binary = :erlang.term_to_binary(deck)
+      File.write(filename, binary)
+    end
+
+    def load(filename) do
+      with {:ok, binary} <- File.read(filename) do
+        :erlang.binary_to_term(binary)
+      else
+        {:error, reason} when reason == :enoent
+          -> "O arquivo nao existe."
+      end
+    end
+
+    def load(filename) do
+      case File.read(filename) do
+        {:ok, binary} ->
+             :erlang.binary_to_term(binary)
+
+        {:error, reason} when reason == :enoent
+           -> "O arquivo nao existe"
+      end
+    end
+
+    def create_hand(hand_size) do
+      create_deck()
+      |> shuffle()
+      |> deal(hand_size)
+    end
 end
