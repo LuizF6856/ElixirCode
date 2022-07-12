734aa96f14e9993ace00a7f7c530502919f98723
LuizF6856 <luizfellipe6843@gmail.com>
2022-07-11 22:18:58 -0300

    [ElixirCode-28] cleaning and documenting the code

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -3,46 +3,60 @@ defmodule Cards do
     Provides methods for creating and hadling a deck of cards
   """
 
-    def create_deck do
-      values = ["Ace", "Two", "Three", "Four", "Five"]
-      suits = ["Tree", "Spades", "Clubs", "Hearts", "Diamonds"]
-
-       for suit <- suits, value <- values, do: "#{value} of #{suit}"
+  @doc """
+    Returns a list of strings representing a deck of playing cards.
+  """
+  def create_deck do
+    for suit <- ["Tree", "Spades", "Clubs", "Hearts", "Diamonds"],
+        value <- ["Ace", "Two", "Three", "Four", "Five"] do
+      "#{value} of #{suit}"
     end
+  end
 
-    def shuffle(deck), do: Enum.shuffle(deck)
+  def shuffle(deck), do: Enum.shuffle(deck)
 
-    def contains?(deck, cards), do: Enum.member?(deck, cards)
+  def contains?(deck, cards), do: Enum.member?(deck, cards)
 
-    def deal(deck, hand_size), do: Enum.split(deck, hand_size)
+  @doc """
+      Divides a deck into a hand and the remainder of the deck.
+      The `hand_size` argument indicates how many cards should
+      be in the hand
 
-    def save(deck, filename) do
-      binary = :erlang.term_to_binary(deck)
-      File.write(filename, binary)
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      => ["Ace of Spades"]
+  """
+  def deal(deck, hand_size), do: Enum.split(deck, hand_size)
+
+  def save(deck, filename) do
+    File.write(filename, :erlang.term_to_binary(deck))
+  end
+
+  def load(filename) do
+    with {:ok, binary} <- File.read(filename) do
+      :erlang.binary_to_term(binary)
+    else
+      {:error, reason} when reason == :enoent ->
+        "O arquivo nao existe."
     end
+  end
 
-    def load(filename) do
-      with {:ok, binary} <- File.read(filename) do
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} ->
         :erlang.binary_to_term(binary)
-      else
-        {:error, reason} when reason == :enoent
-          -> "O arquivo nao existe."
-      end
-    end
 
-    def load(filename) do
-      case File.read(filename) do
-        {:ok, binary} ->
-             :erlang.binary_to_term(binary)
-
-        {:error, reason} when reason == :enoent
-           -> "O arquivo nao existe"
-      end
+      {:error, reason} when reason == :enoent ->
+        "O arquivo nao existe"
     end
+  end
 
-    def create_hand(hand_size) do
-      create_deck()
-      |> shuffle()
-      |> deal(hand_size)
-    end
+  def create_hand(hand_size) do
+    create_deck()
+    |> shuffle()
+    |> deal(hand_size)
+  end
 end
